655bbfcaa596f035ace5e0399c3ad1df5beb0fa0
KhoiVu <tuankhoi2206@gmail.com>
2020-06-14 17:46:15 +0700

    add static pages.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,20 @@
 import express from "express";
 import connectDB from "./config/connect.db";
 import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine"
 
 const hostname = "localhost";
 const port = 8017;
 let app = express();
 connectDB();
+configViewEngine(app);
 
-app.get('/hello-world', async function (req, res) {
-    try {
-        let item = {
-            userId: "06142020",
-            contactId: "06142020",
-        };
-        let contact= await ContactModel.createNew(item);
-        res.send(contact);
-    } catch (e) {
-        console.log(err);
-    }
+app.get('/', function (req, res) {
+    return res.render("main/master");
+});
 
-
+app.get('/login-register', function (req, res) {
+    return res.render("auth/loginRegister");
 });
 
 app.listen(port, hostname, function () {
