39360249fe22c7a5267d0e4e46dda4150bee3669
KhoiVu <tuankhoi2206@gmail.com>
2020-06-21 00:55:13 +0700

    add-contact-realtime

--- src/sockets/contact/addNewContact.js
+++ src/sockets/contact/addNewContact.js
@@ -1,16 +1,58 @@
-import UserModel from "./../../models/userModel";
-
 /**
  * @param io from socket.io lib
  */
 let addNewContact = (io) => {
-    io.on("connection", (socket) => {
-        socket.on("add-new-contact", async (data) => {
 
-            console.log('add-new-contact');
-            console.log(data);
-            console.log(socket.request.user);
+    let clients = {};
+    io.on('connection', (socket) => {
 
+        let currentUserId = socket.request.user._id;
+
+        console.log('***************************');
+        console.log('connection');
+        console.log('UserId: ' + socket.id);
+        console.log('UserId: ' + currentUserId);
+        console.log('UserName: ' + socket.request.user.username);
+        console.log('***************************');
+
+        if (clients[currentUserId]) {
+            clients[currentUserId].push(socket.id);
+        } else {
+            clients[currentUserId] = [socket.id];
+        }
+
+        socket.on('add-new-contact', async (data) => {
+
+            /**
+             * to receive contactId which we want to send notification.
+             */
+            let currentUser = {
+                id: socket.request.user._id,
+                username: socket.request.user.username,
+                avatar: socket.request.user.avatar,
+            };
+            console.log('currentUser');
+            console.log(currentUser);
+
+            /**
+             * send notification to socketId of contactId which we want to send
+             */
+            if (clients[data.contactId]) {
+                clients[data.contactId].forEach(socketId => {
+                    console.log('socketId' + socketId);
+                    io.sockets.connected[socketId].emit('response-add-new-contact', currentUser);
+                });
+            }
+        });
+
+        socket.on('disconnect', () => {
+
+            if (clients[currentUserId]) {
+                clients[currentUserId] = clients[currentUserId].filter((socketId) => socketId !== socket.id);
+                if (!clients[currentUserId].length) {
+                    delete clients[currentUserId];
+                }
+            }
         });
     });
 }
---
+++ src/sockets/contact/removeRequestContact.js
@@ -0,0 +1,42 @@
+/**
+ * @param io from socket.io lib
+ */
+let removeRequestContact = (io) => {
+
+    let clients = {};
+    io.on("connection", (socket) => {
+
+        let currentUserId = socket.request.user._id;
+        socket.on('remove-request-contact', async (data) => {
+
+            if (clients[currentUserId]) {
+                clients[currentUserId].push(socket.id);
+            } else {
+                clients[currentUserId] = [socket.id];
+            }
+
+            let currentUser = {
+                id: socket.request.user._id,
+                username: socket.request.user.username,
+                avatar: socket.request.user.avatar,
+            };
+
+            if (clients[data.contactId]) {
+                clients[data.contactId].forEach(socketId => {
+                    io.sockets.connected[socketId].emit('response-remove-request-contact', currentUser);
+                });
+            }
+        });
+
+        socket.on('disconnect', () => {
+
+            if (clients[currentUserId]) {
+                clients[currentUserId] = clients[currentUserId].filter((socketId) => socketId !== socket.id);
+                if (!clients[currentUserId].length) {
+                    delete clients[currentUserId];
+                }
+            }
+        });
+    });
+}
+module.exports = removeRequestContact;
